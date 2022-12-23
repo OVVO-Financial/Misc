4c31933be0201f2cee972eab01d143d36940071f
OVVO-Financial <ovvo.financialsystems@gmail.com>
2022-12-23 11:54:09 -0500

    Update publish_live_quotes_to_streams.R

--- publish_live_quotes_to_streams.R
+++ publish_live_quotes_to_streams.R
@@ -1,12 +1,8 @@
 Sys.setenv(TZ='America/New_York')
 
-library(quantmod) # For quote downloads
-library(httr) # If you want to publish prices as streams
-library(data.table) # If you want to store the values locally efficiently, can append, let me know...
-
 yarx_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/xraytickers.json")
 yarx_securities = toupper(as.character(unlist(yarx_securities_raw)))
-    
+
 rdps_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/rdpstickers.json")
 rdps_securities = toupper(as.character(unlist(rdps_securities_raw)))
 
@@ -20,35 +16,34 @@ repeat{
   if(!weekend){
     if((data.table::hour(start.time)==9 && data.table::minute(start.time)>=30) ||  
        (data.table::hour(start.time)>=10 && Sys.time() - quantmod::getQuote("^GSPC", what = yahooQF("marketstate")))){    
-    combined_quotes = data.table::data.table(quantmod::getQuote(combined_securities, what = yahooQF("Last Trade (Price Only)")))
-    combined_quotes$ID = combined_securities
-    print(combined_quotes)
-    
-    # Uncomment to publish streams with .../AAPL_prices.json format for `sponsor_write_key` 
-    for(i in 1:length(combined_securities)){
-      httr::PUT(url = paste0("https://api.microprediction.org/live/", combined_securities[i], "_prices.json"),
-                body = list(write_key = sponsor_write_key, budget = 1, value = combined_quotes$Last[i]))
-    }
-    
-    eclipsed = as.numeric(Sys.time()-start.time, units="secs")
-    
-    print(paste("Resting for: ", max(0, 60-eclipsed)))
-    
-    Sys.sleep(max(0, 60-eclipsed))
-  } else {
-     if(data.table::minute(start.time)>=59){  
+      combined_quotes = data.table::data.table(quantmod::getQuote(combined_securities, what = yahooQF("Last Trade (Price Only)")))
+      combined_quotes$ID = combined_securities
+      print(combined_quotes)
+      
+      # Uncomment to publish streams with .../AAPL_prices.json format for `sponsor_write_key` 
+      for(i in 1:length(combined_securities)){
+        httr::PUT(url = paste0("https://api.microprediction.org/live/", combined_securities[i], "_prices.json"),
+                  body = list(write_key = sponsor_write_key, budget = 1, value = combined_quotes$Last[i]))
+      }
+      
+      eclipsed = as.numeric(Sys.time()-start.time, units="secs")
+      
+      print(paste("Resting for: ", max(0, 60-eclipsed)))
+      
+      Sys.sleep(max(0, 60-eclipsed))
+    } else {
+      if(data.table::minute(start.time)>=59){  
         yarx_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/xraytickers.json")
         yarx_securities = toupper(as.character(unlist(yarx_securities_raw)))
-    
+        
         rdps_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/rdpstickers.json")
         rdps_securities = toupper(as.character(unlist(rdps_securities_raw)))
-
+        
         combined_securities = c(yarx_securities, rdps_securities)
-          
+        
         print(paste0("Market closed: ", start.time))
         Sys.sleep(60)
-     }
-  }
- }      
+      }
+    }
+  }      
 }
-

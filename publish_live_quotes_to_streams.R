bd9a55948b6f0ae159639e3f86912d6af5963f87
OVVO-Financial <ovvo.financialsystems@gmail.com>
2022-12-23 10:13:34 -0500

    Time bug

--- publish_live_quotes_to_streams.R
+++ publish_live_quotes_to_streams.R
@@ -16,7 +16,7 @@ sponsor_write_key = "6a1248f2cf43d78cebb54861fea2401c"
 
 repeat{
   start.time = Sys.time()
-  if(data.table::hour(start.time)>=9 && data.table::minute(start.time)>=30 && data.table::hour(start.time)<16){    
+  if((data.table::hour(start.time)==9 && data.table::minute(start.time)>=30) ||  (data.table::hour(start.time)>=10 && data.table::hour(start.time)<16)){    
     combined_quotes = data.table::data.table(quantmod::getQuote(combined_securities, what = yahooQF("Last Trade (Price Only)")))
     combined_quotes$ID = combined_securities
     print(combined_quotes)
@@ -32,6 +32,19 @@ repeat{
     print(paste("Resting for: ", max(0, 60-eclipsed)))
     
     Sys.sleep(max(0, 60-eclipsed))
-  } else {if(data.table::minute(start.time)>=59)  print(paste0("Market closed: ", start.time)); Sys.sleep(60)}
+  } else {
+      if(data.table::minute(start.time)>=59){  
+            yarx_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/xraytickers.json")
+            yarx_securities = toupper(as.character(unlist(yarx_securities_raw)))
+    
+            rdps_securities_raw = jsonlite::fromJSON("https://raw.githubusercontent.com/microprediction/microprediction/master/microprediction/live/rdpstickers.json")
+            rdps_securities = toupper(as.character(unlist(rdps_securities_raw)))
+
+            combined_securities = c(yarx_securities, rdps_securities)
+          
+            print(paste0("Market closed: ", start.time))
+            Sys.sleep(60)
+      }
+  }    
 }
 
